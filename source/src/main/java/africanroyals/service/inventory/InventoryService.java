084055a8b2626a45199a1a28e37ac60293436a3e
aaliy4hscode <uchenduaaliyah@gmail.com>
2025-12-09 18:38:33 -0500

    Define InventoryService interface with methods

--- source/src/main/java/africanroyals/service/inventory/InventoryService.java
+++ source/src/main/java/africanroyals/service/inventory/InventoryService.java
@@ -1,1 +1,9 @@
+package com.africanroyals.service.inventory;
 
+import com.africanroyals.entity.InventoryItem;
+import java.util.List;
+
+public interface InventoryService {
+    List<InventoryItem> getAvailableItems();
+    List<InventoryItem> searchItems(String query);
+}
